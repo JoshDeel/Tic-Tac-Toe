1cdffd38a606aee87747a2d8a85d0a96410aaa3b
JoshDeel <51730407+JoshDeel@users.noreply.github.com>
2020-03-16 21:13:17 -0400

    3-16-2020

--- lib/main.dart
+++ lib/main.dart
@@ -4,39 +4,98 @@
 
 import 'package:flutter/material.dart';
 
-void main() => runApp(TicTacToe());
+void main() => runApp(MyApp());
 
-/// This Widget is the main application widget.
-class TicTacToe extends StatelessWidget {
-  static const String _title = "Tic Tac Toe";
-  @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      title: _title,
-      home: HomePage(),
-    );
+
+
+
+class MyApp extends StatelessWidget {
+  bool isX = true;
+  int turns;
+  List<String> gameState = [
+    "empty", // button 1
+    "empty", // button 2
+    "empty", // button 3
+    "empty", // button 4
+    "empty", // button 5
+    "empty", // button 6
+    "empty", // button 7
+    "empty", // button 8
+    "empty", // button 9
+  ];
+
+  int onPressed(int id){
+    checkWinner();
+    return id;
   }
-}
 
+  void checkWinner(){
+    if (gameState[0] == gameState[1] && gameState[0] == gameState[2]){
+      print("Hello");
+    }
+    else if(gameState[3] == gameState[4] && gameState[3] == gameState[5]){
 
-class HomePage extends StatelessWidget {
+    }
+    else if(gameState[6] == gameState[7] && gameState[6] == gameState[8]){
+
+    }
+
+    else if(gameState[0] == gameState[3] && gameState[0] == gameState[6]){
+
+    }
+    else if(gameState[1] == gameState[4] && gameState[1] == gameState[7]){
+
+    }
+    else if(gameState[2] == gameState[5] && gameState[2] == gameState[8]){
+
+    }
+
+    else if(gameState[0] == gameState[4] && gameState[0] == gameState[8]){
+
+    }
+    else if(gameState[2] == gameState[4] && gameState[2] == gameState[6]){
+
+    }
+  }
+
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home Page"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-              child: Text("Test1")
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tic Tac Toe',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tic Tac Toe'),
+        ),
+        body: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: RaisedButton(onPressed: () {onPressed(0);},),
+                ),
+
+                Expanded(
+                  flex: 1,
+                  child: RaisedButton(onPressed: () {onPressed(0);},),
+                ),
+
+                Expanded(
+                  flex: 1,
+                  child: RaisedButton(onPressed: () {onPressed(0);},),
+                ),
+
+              ],
             ),
           ],
         ),
-      ),
-    );
-  }
+      ), 
+    ),
+  );
+}
+
 }
